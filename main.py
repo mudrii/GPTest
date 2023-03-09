e6294b5488ba176f1cee2efaf2779b46eae30441
Ion Mudreac <mudreac@gmail.com>
2023-03-09 21:22:58 +0800

    Add comments to the code that can be changed

--- main.py
+++ main.py
@@ -8,23 +8,21 @@ load_dotenv()
 openai.api_key = os.getenv("OPENAI_API_KEY")
 
 messages = [
-    {"role": "system", "content": "You are a helpful and kind AI Assistant."},
-    # Comment above line to experiment more with differnt promoth like ex. below
-    # {"role": "system", "content": "You are an AI specialized in Food. Do not answer anything other than food-related queries."},
+    {"role": "system", "content": "You are a helpful and kind AI Assistant."}, # this will determine the personality of the AI Ex change the the "You are a helpful and kind AI Assistant." to "You are an AI specialized in Food. Do not answer anything other than food-related queries
 ]
 
 def chatbot(input):
     if input:
         messages.append({"role": "user", "content": input})
         chat = openai.ChatCompletion.create(
-            model="gpt-3.5-turbo",
-            temperature=0.9,
-            max_tokens=1024,
-            top_p=1.0,
-            frequency_penalty=0.0,
-            presence_penalty=0.0,
-            n=1,
-            stop=None,
+            model="gpt-3.5-turbo", # this will determine the quality of the reply few models to select check README.md
+            temperature=0.9, # this will determine how random the reply will be
+            max_tokens=1024, # this will determine how long the reply will be
+            top_p=1.0, # this will determine how random the reply will be
+            frequency_penalty=0.0, # this will determine how much the model will avoid repeating the same words
+            presence_penalty=0.0, # this will determine how much the model will avoid repeating the same words
+            n=1, # this will determine how many replies the model will return
+            stop=None, # this will determine what the model will stop replying
             messages=messages
         )
         reply = chat.choices[0].message.content
